8bcd530c10d2d233720b877244281a0aba753818
Frank Sam <fsh1991@hotmail.com>
2017-08-01 22:05:21 -0600

    Added stars, moves and time functionality. Replaced classes that was only declared one time to id

--- assets/js/memory_game.js
+++ assets/js/memory_game.js
@@ -4,19 +4,19 @@
 	/*Preloads images so it will appear inmediately when user clicks on it*/
 	for (let i = 0; i < 8; i++) {
 		let pandaImage = new Image()
-		pandaImage.src = "assets/img/"+(i+1)+".gif";
+		pandaImage.src = "assets/img/" + (i + 1) + ".gif";
 	}
-	
-	let memoryCardPositions = [[], [], [], []], /*Stores the pair random numbers for each card*/
-		firstCard = {}; /*Stores the first clicked card for each pair of cards*/
-	
+
+	let memoryCardPositions = [[], [], [], []],//Stores the pair random numbers for each card
+		firstCard = {}; //Stores the first clicked card for each pair of cards
+
 	/*Checks if the random number dont have been used or dont have a pair*/
 	let isValidNumber = (number) => {
 		let cardNumberREgExp = new RegExp(number, "g");
 		let numberMatches = memoryCardPositions.toString().match(cardNumberREgExp);
 		return numberMatches === null ? true : numberMatches.length < 2
 	}
-	
+
 	/*Generates the random numbers for each card*/
 	let poblateRandomCardsPosition = () => {
 		for (let x = 0; x < 4; x++) {
@@ -33,58 +33,98 @@
 		}
 	}
 	poblateRandomCardsPosition();
-	
+
 	/*******************************************************UI SECTION************************************/
 	let cleanPickedCardsTimer = 0,
-		matchedPair = false;
+		matchedPair = false,
+		runTimer = true,
+		timer = 0,
+		seconds = 0,
+		moves = 0;
+
+	let checkMovesAmmount = () =>{
+		moves++;
+		$("#movesAmount").text(moves);
+		if (moves === 11 || moves === 15) { //after 10 or 14 (not inclusive) moves, remove 1 star
+			$("#starsAmount").fadeOut(function () {
+				this.innerHTML = this.innerHTML.slice(0, -1);
+			}).fadeIn();
+		}
+	}
 	
+	/**/
+	let executeElapsedTime = () =>{
+		runTimer = false;
+		timer = setInterval(() => {
+			seconds++;
+			$("#timer").html(seconds + " sec");
+		}, 1000)
+	}
+
 	/*When user clicks a card event*/
 	$(".card").click(function () {
+		if (runTimer) {
+			executeElapsedTime();
+		}
 		let thisCard = $(this),
-			cardHorizontalPosition= thisCard.index(),
+			cardHorizontalPosition = thisCard.index(),
 			cardVerticalPostion = thisCard.parent().index(),
 			value = memoryCardPositions[cardVerticalPostion][cardHorizontalPosition];
-		if($(".pickedCard").length>1) {
+		/*Instant hide card in case the timer haven't been executed*/
+		if ($(".pickedCard").length > 1) {
 			clearTimeout(cleanPickedCardsTimer)
-			if(!matchedPair) $(".pickedCard").css("background-image","")
+			if (!matchedPair) $(".pickedCard").css("background-image", "")
 			$(".pickedCard").removeClass("pickedCard");
 			firstCard = {};
 		}
-		if(!thisCard.hasClass("pickedCard")&&thisCard.css("background-image").indexOf("cardBack")!==-1){
-			thisCard.css("background-image","url(assets/img/"+value+".gif)");
+		if (!thisCard.hasClass("pickedCard") && thisCard.css("background-image").indexOf("cardBack") !== -1) {
+			thisCard.css("background-image", "url(assets/img/" + value + ".gif)");
 			thisCard.addClass("pickedCard");
 			/*First click for each pair*/
-			if(!firstCard.value){
+			if (!firstCard.value) {
 				firstCard.value = value;
 				firstCard.dom = thisCard;
-			}else{/*Second click for each pair*/
-				if(firstCard.value === value){/*is a pair*/
+			} else { /*Second click for each pair*/
+				checkMovesAmmount();
+				if (firstCard.value === value) { /*is a pair*/
 					matchedPair = true;
-					thisCard.addClass("pairFounded")
-					firstCard.dom.addClass("pairFounded")
+					thisCard.addClass("pairFounded");//adding flags to know how much pair of cards has been found
+					firstCard.dom.addClass("pairFounded");
 					$(".pickedCard").removeClass("pickedCard");
 					firstCard = {}
-					if($(".pairFounded").length===16){
-						$(".modal, .winMessage").fadeIn();
+					/*Game completed*/
+					if ($(".pairFounded").length === 16) {
+						$("#modal, #winMessage").fadeIn();
+						let earnedStars = $("#starsAmount").text();
+						$("#stats").html("Total Time: "+seconds+" seconds<br>Total Moves: " + moves + "<br>Stars: " + earnedStars)
 					}
-				}else{
+				} else {
 					matchedPair = false;
-					cleanPickedCardsTimer = setTimeout(function(){
-						$(".pickedCard").css("background-image","")
+					/*Timer to auto hide cards*/
+					cleanPickedCardsTimer = setTimeout(function () {
+						$(".pickedCard").css("background-image", "")
 						$(".pickedCard").removeClass("pickedCard");
 						firstCard = {};
-					},500)
+					}, 1000)
 				}
 			}
 		}
 	});
-	/*When users click new game button*/
-	$(".new_game").click(function(){
-		$(".modal").fadeOut();
-		$("#startMessage").css("display","none");
-		memoryCardPositions = [[], [], [], []];
+	/*When users click new game button reset all values to their default value*/
+	$(".new_game").click(function () {
+		$("#modal").fadeOut();
+		$("#startMessage").css("display", "none");
+		$(".card").css("background-image", "");
+		$("#starsAmount").html("★★★");
+		$("#movesAmount").html("0");
+		$("#timer").html(0);
+		$(".pairFounded").removeClass("pairFounded");
+		clearInterval(timer);
 		firstCard = {};
-		$(".card").css("background-image","");
+		memoryCardPositions = [[], [], [], []];
+		runTimer = true;
+		seconds = 0;
+		moves = 0;
 		poblateRandomCardsPosition();
 	});
 })()
